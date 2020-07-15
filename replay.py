6d9a98c572f7dac6e9b8518c865fa20d6ad1cee7
Gekuan-Du <63496847+Gekuan-Du@users.noreply.github.com>
2020-07-15 18:06:38 +0800

    Create replay.py

---
+++ replay.py
@@ -0,0 +1,18 @@
+from chinachess import *
+import table
+import time
+import numpy
+file = open('chinachess.txt','r')
+move_list = file.readlines()
+file.close()
+for i in range(len(move_list)) :
+    line = move_list[i][:-1]
+    move_list[i] = line
+table = table.table
+print_table(table)
+for i in range(len(move_list)) :
+    start = move_list[i][:2]
+    end = move_list[i][2:]
+    table = move(table,start,end)
+    print_table(table)
+    time.sleep(3)
